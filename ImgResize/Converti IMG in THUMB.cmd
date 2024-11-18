6897b0293b18307c6aa68dd8976960a4a9286363
Mattia Caneva <canvacanva@gmail.com>
2024-11-18 12:02:12 +0100

    Add files via upload

---
+++ ImgResize/Converti IMG in THUMB.cmd
@@ -0,0 +1,1 @@
+powershell ./Launcher.ps1
---
+++ ImgResize/Launcher.ps1
@@ -0,0 +1,30 @@
+﻿$source = 'C:\Portable\ImgResize\images'
+$dest = 'C:\Portable\ImgResize\thumb'
+$source_list = get-childitem -path 'C:\Portable\ImgResize\images' -recurse
+$dest_list = get-childitem -Path 'C:\Portable\ImgResize\thumb' -recurse
+	
+$source_test = Test-Path -Path $source\*
+$dest_test = Test-Path -Path $dest\*
+#Test-Path -Path $source\*
+#Test-Path -Path $dest\*
+
+IF ($source_test -eq $false){
+Write-Host "Nessun file presente nella cartella "$source\
+exit
+}
+
+IF ($dest_test -eq $false){
+Write-Host "Nessun file presente nella cartella "$dest\ " --> creare almeno un file txt di prova"
+exit
+}
+
+$cObj = compare-object -DifferenceObject $source_list -ReferenceObject $dest_list -IncludeEqual
+foreach ($obj in $cObj) {
+echo $obj
+echo $cObj
+if ($obj.sideindicator -ne "=="){
+echo "$($obj.InputObject.FullName)"
+echo "$($obj.InputObject)"
+      ./Resizer.ps1 $source\$($obj.InputObject) $dest\$($obj.InputObject) 100
+}
+}
